79833968798a8eff398933899f08aa2e6d4c40d3
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-01 21:14:02 -0800

    Add karma config

--- js/triangle.js
+++ js/triangle.js
@@ -3,3 +3,10 @@ function Triangle(side1, side2, side3) {
   this.side2 = side2;
   this.side3 = side3;
 }
+exports.triangleModule = Triangle;
+
+Triangle.prototype.checkType = function() {
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+    return "not a triangle";
+  }
+};
